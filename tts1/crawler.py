fe02226e8e65f002842b18ceef87a21cbefb6d67
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2012-09-27 14:57:05 +0100

    Beginning tts1

---
+++ tts1/crawler.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Web crawler for TTS Assignment 1
+# Steven Eardley s0934142
+
+import re
+import urllib
+import robotparser
+import heapq
+
+# Nab all permitted URLs on a page
+def grabURLs(page):
+	# TODO use robotparser to check permissions
+
+# Add a page to our frontier queue
+def addPage(page,queue):
+	# do something
+
+
+

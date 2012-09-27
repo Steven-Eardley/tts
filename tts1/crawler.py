7b8ae84927cb88f1d9e6f3d99234f2a35a5024fc
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2012-09-27 17:54:27 +0100

    work on tts1

--- tts1/crawler.py
+++ tts1/crawler.py
@@ -7,13 +7,32 @@ import urllib
 import robotparser
 import heapq
 
-# Nab all permitted URLs on a page
+useragent = "TTS"
+rp = robotparser.RobotFileParser()
+
+startpage = "http://ir.inf.ed.ac.uk/tts/A1/0934142/0934142.html"
+
+def setUpRobot(robotURL):
+	rp.set_url(robotURL)
+	rp.read()
+	rp.modified()
+
+def loadPage(url):
+	if rp.can_fetch(useragent,url):
+		return page = urllib.urlopen(url)
+	else:
+		return None
+
+# Nab URLs between CONTENT comments on a page
 def grabURLs(page):
-	# TODO use robotparser to check permissions
+	if page != None
+	return 0
 
 # Add a page to our frontier queue
 def addPage(page,queue):
-	# do something
+	return 0
 
+setUpRobot("http://ir.inf.ed.ac.uk/robots.txt")
 
+loadPage(startpage)
 

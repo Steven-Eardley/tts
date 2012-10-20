5f075d7ab1c143851f4901fbb6f656e91de5b491
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2012-10-20 18:52:59 +0100

    some work has been done

--- tts2/tfidf.py
+++ tts2/tfidf.py
@@ -3,6 +3,10 @@
 # Steven Eardley s0934142
 
 import re
+import math
+
+# Save the dfw data to ease computation
+dfwDict = dict()
 
 # Return data from a file as a list of (number, [keyword]) tuples
 def getData(filename):
@@ -21,32 +25,64 @@ def getData(filename):
 		parsedData.append((number, words))
 	return parsedData
 
-def evalTfidf(query, document):
-	similarity = 0
-	
-	# Remove duplicates from the document
-	docSet = list(set(document[1]))
-	
-	# If the keyword appears in the set, increment the similarity score.
-	for keyword in query[1]:
-		if docSet.count(keyword) > 0:
-			similarity += 1
-
-	if similarity > 0:
-		report = '{0} 0 {1} 0 {2} 0'.format(query[0], document[0], similarity)
-		return report
+def findDfw(word,documents):
+	if word not in dfwDict:
+		dfw = len([1 for document in documents if document[1].count(word) > 0])
+		dfwDict[word] = dfw
+		return dfw
 	else:
-		return None
+		return dfwDict[word]
 
-outFile = open('overlap.top', 'w')
+def evalTfidf(query, documents, c, k, avgD):
+	reports = []
+	
+	for document in documents:
+		similarity = 0
+		
+		# The set of unique query words
+		queryVocab = list(set(query[1]))
+		
+
+		for keyword in queryVocab:
+			
+			# The number of occurances of the keyword in the query
+			tfwq = float(query[1].count(keyword))
+			
+			# The number of occurances of the keyword in the document
+			tfwd = float(document[1].count(keyword))
+			
+			# The number of occurances of the keyword in all documents
+			dfw = float(findDfw(keyword, documents))
+
+			# |D| The document length
+			d = float(len(document[1]))
+			
+			# tf.idf weighted sum
+			similarity += (tfwq * (tfwd / (tfwd + ((k * d) / avgD))) *
+math.log(c / dfw))
+
+		if similarity > 0:
+			report = '{0} 0 {1} 0 {2} 0'.format(query[0], document[0],
+similarity)
+			reports.append(report)
+	return reports
+
+outFile = open('tfidf.top', 'w')
 queries = getData('qrys.txt')
 documents = getData('docs.txt')
 
+# |C| The total number of documents
+c = float(len(documents))
+
+# avg|D| The mean document length
+avgD = float(sum([len(doc[1]) for doc in documents]) / c)
+
+k = 2.0
+
 for query in queries:
-	for document in documents:
-		result = evalTfidf(query, document)
-		if result != None:
-			outFile.write(result + '\n')
+	results = evalTfidf(query, documents, c, k, avgD)
+	for result in results:
+		outFile.write(result + '\n')
 outFile.close()
 
 

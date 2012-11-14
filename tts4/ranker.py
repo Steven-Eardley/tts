4a204c48283d3b1dc0a780f2e04438274477b56f
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2012-11-14 15:38:54 +0000

    ranker reads file

--- tts4/ranker.py
+++ tts4/ranker.py
@@ -2,6 +2,8 @@
 # Ranking program for Text Technologies Assignment 4
 # Steven Eardley s0934142
 
+from sys import argv, exit
+
 if len(argv) != 2:
     print "\nUse: ranker.py inputFile.txt\n"
     exit()
@@ -11,3 +13,15 @@ hubs = open('hubs.txt', 'w')
 auth = open('auth.txt', 'w')
 pr = open('pr.txt', 'w')
 
+def createGraph():
+    f = open(argv[1], 'r')
+    try:
+        rawData = f.readlines()
+    finally:
+        f.close()
+    
+    for line in rawData[:10]:
+        [msg_id, sender, recipient] = line.split()
+        print (sender, recipient)
+
+createGraph()

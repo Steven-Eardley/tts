24a1d8000e2a8b818aeba6b3415066cbe77511f1
Steven Eardley <me@steveneardley.co.uk>
2012-11-16 17:03:10 +0000

    some more work with ranker

--- tts4/ranker.py
+++ tts4/ranker.py
@@ -16,6 +16,9 @@ pr = open('pr.txt', 'w')
 # graph_info is a dict: {sender : ([outgoing], [incoming])}
 graph_info = dict()
 
+# PageRank info in a dict: {sender : PageRank_score}
+scores = dict()
+
 def createGraph():
     f = open(argv[1], 'r')
     try:
@@ -25,20 +28,34 @@ def createGraph():
     
     for line in rawData:
         [msg_id, sender, recipient] = line.split()
-        try:
-            graph_info[sender][0].add(recipient)
-        except KeyError:
-            graph_info[sender] = (set([recipient]), set())
+        if recipient != sender:
+            try:
+                graph_info[sender][0].add(recipient)
+            except KeyError:
+                graph_info[sender] = ([recipient], [])
             
-        try:
-            graph_info[recipient][1].add(sender)
-        except KeyError:
-            graph_info[recipient] = (set(), set([sender]))
+            try:
+                graph_info[recipient][1].add(sender)
+            except KeyError:
+                graph_info[recipient] = ([], [sender])
+            
+def pagerank(iterations, lmbda):
+    n = len(graph_info)
+    for i in range(0,iterations):
+        for (person, (outgoing, incoming)) in graph_info.items():
+            for inc in incoming:
+                try:
+                    
+            neighbour_scores = [
+            scores[person] = (1 - lmbda) / n + lmbda * sum(
+            
+    
 
 createGraph()
-for (k, (o,i)) in graph_info.items():
-    print "ADDRESS %s" % k
-    print "OUTGOING"
-    print list(o)
-    print "INCOMING"
-    print list(i)
+
+#for (k, (o,i)) in graph_info.items():
+#    print "ADDRESS %s" % k
+#    print "OUTGOING"
+#    print list(o)
+#    print "INCOMING"
+#    print list(i)

cd58a87609bf6e81cc2e558d10188f91234e5edc
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2012-11-16 19:10:03 +0000

    pagerank almost works. ish.

--- tts4/ranker.py
+++ tts4/ranker.py
@@ -3,6 +3,7 @@
 # Steven Eardley s0934142
 
 from sys import argv, exit
+import operator
 
 if len(argv) != 2:
     print "\nUse: ranker.py inputFile.txt\n"
@@ -16,9 +17,6 @@ pr = open('pr.txt', 'w')
 # graph_info is a dict: {sender : ([outgoing], [incoming])}
 graph_info = dict()
 
-# PageRank info in a dict: {sender : PageRank_score}
-scores = dict()
-
 def createGraph():
     f = open(argv[1], 'r')
     try:
@@ -30,28 +28,40 @@ def createGraph():
         [msg_id, sender, recipient] = line.split()
         if recipient != sender:
             try:
-                graph_info[sender][0].add(recipient)
+                graph_info[sender][0].append(recipient)
             except KeyError:
                 graph_info[sender] = ([recipient], [])
             
             try:
-                graph_info[recipient][1].add(sender)
+                graph_info[recipient][1].append(sender)
             except KeyError:
                 graph_info[recipient] = ([], [sender])
             
 def pagerank(iterations, lmbda):
-    n = len(graph_info)
+    n = float(len(graph_info))
+    
+    init_score = 1.0 / n
+    
+    # Initialise PageRank score in a dict: {sender : PageRank_score}
+    scores = dict(zip(graph_info.keys(), [init_score]*len(graph_info)))
+    
     for i in range(0,iterations):
         for (person, (outgoing, incoming)) in graph_info.items():
+            # Sum the scores of incoming links to our person
+            total_inc_score = 0.0
             for inc in incoming:
-                try:
-                    
-            neighbour_scores = [
-            scores[person] = (1 - lmbda) / n + lmbda * sum(
+                                         # divide score by no. of outgoing links
+                 total_inc_score += scores[inc] / float(len(graph_info[inc][0]))
             
+            scores[person] = (1 - lmbda) / n + lmbda * total_inc_score
+    return scores
     
 
 createGraph()
+score = pagerank(10, 0.8)
+for (person, score) in sorted(score.iteritems(), key=operator.itemgetter(1), reverse = True):
+    print "%.8f" % score,
+    print person
 
 #for (k, (o,i)) in graph_info.items():
 #    print "ADDRESS %s" % k
@@ -59,3 +69,5 @@ createGraph()
 #    print list(o)
 #    print "INCOMING"
 #    print list(i)
+
+# using Decimal slowed it down too much
